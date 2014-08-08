1b4c5e4c698ec6986db635a9c53285e5aac7aa8e
Jack+Sam <Jack+Sam@appacademy.io>
2014-08-08 10:32:18 -0400

    Adds user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,35 @@
 class User < ActiveRecord::Base
+  validates :username, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user && user.has_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def has_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom::urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_token
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+  end
+
 end
--- db/migrate/20140808133851_create_users.rb
+++ db/migrate/20140808133851_create_users.rb
@@ -7,5 +7,7 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    add_index :users, :username, unique: true
   end
 end
