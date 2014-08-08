0e5baed9dafd3df26e06cd243cfcd5c06977253e
Jack+Sam <Jack+Sam@appacademy.io>
2014-08-08 11:04:35 -0400

    creates new user and session views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       sign_in(@user)
-      redirect_to goals_url
+      render json: {"ok" => 45}
     else
-      flash.now(:errors) = @user.errors.full_messages
+      flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :password)
   end
-
-
 end
